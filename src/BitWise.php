dc7bf017898f248e6634106212f8592ab19ee204
Brooke Bryan <brooke@bajb.net>
2015-10-07 23:47:04 +0100

    Bitwise Bits

--- src/BitWise.php
+++ src/BitWise.php
@@ -18,4 +18,51 @@ class BitWise
     }
     return $bit > 0 && bcmod($bit, 2) == 0 && ($bit & ($bit - 1)) == 0;
   }
+
+  public static function remove($mask, $bit)
+  {
+    return ($mask & $bit) ? ($mask ^ $bit) : $mask;
+  }
+
+  public static function add($mask, $bit)
+  {
+    return $mask | $bit;
+  }
+
+  public static function toggle($mask, $bit)
+  {
+    if($mask & $bit)
+    {
+      return $mask ^ $bit;
+    }
+    else
+    {
+      return $mask | $bit;
+    }
+  }
+
+  public static function has($mask, $bit)
+  {
+    return ($mask & $bit) === $bit;
+  }
+
+  public static function getBits($mask)
+  {
+    $bits = [];
+    for($i = 1; $i <= $mask; $i = $i * 2)
+    {
+      if($i & $mask)
+      {
+        $bits[] = $i;
+      }
+    }
+
+    return $bits;
+  }
+
+  public static function highest($mask)
+  {
+    $bits = static::getBits($mask);
+    return end($bits);
+  }
 }
--- tests/BitWiseTest.php
+++ tests/BitWiseTest.php
@@ -4,6 +4,13 @@ use Packaged\Helpers\BitWise;
 
 class BitWiseTest extends PHPUnit_Framework_TestCase
 {
+  const ONE = 1;
+  const TWO = 2;
+  const THREE = 4;
+  const FOUR = 8;
+  const FIVE = 16;
+  const SIX = 32;
+
   /**
    * @large
    */
@@ -28,5 +35,45 @@ class BitWiseTest extends PHPUnit_Framework_TestCase
       $this->assertTrue(BitWise::isSingleBit($checkBit));
       $this->assertFalse(BitWise::isSingleBit(bcsub($checkBit, 3)));
     }
+  }
+
+  public function testModify()
+  {
+    $state = 0;
+
+    //Has
+    $this->assertFalse(BitWise::has($state, static::ONE));
+
+    //Add
+    $state = BitWise::add($state, static::ONE);
+    $this->assertTrue(BitWise::has($state, static::ONE));
+    $state = BitWise::add($state, static::TWO);
+    $state = BitWise::add($state, static::TWO);
+    $this->assertTrue(BitWise::has($state, static::ONE));
+    $this->assertTrue(BitWise::has($state, static::TWO));
+
+    //Remove
+    $state = BitWise::remove($state, static::ONE);
+    $state = BitWise::remove($state, static::ONE);
+    $this->assertTrue(BitWise::has($state, static::TWO));
+    $this->assertFalse(BitWise::has($state, static::ONE));
+
+    //Toggle
+    $state = BitWise::toggle($state, static::ONE);
+    $this->assertTrue(BitWise::has($state, static::ONE));
+    $state = BitWise::toggle($state, static::ONE);
+    $this->assertFalse(BitWise::has($state, static::ONE));
+
+    //Highest
+    $state = BitWise::add($state, static::FOUR);
+    $this->assertEquals(static::FOUR, BitWise::highest($state));
+    $state = BitWise::add($state, static::SIX);
+    $this->assertEquals(static::SIX, BitWise::highest($state));
+
+    //Get Bits
+    $this->assertEquals(
+      [static::TWO, static::FOUR, static::SIX],
+      BitWise::getBits($state)
+    );
   }
 }
