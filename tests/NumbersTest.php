3c135848590cb906886efc1a6f06a7e0c8928e9a
Tom Kay <oridan82@gmail.com>
2016-06-17 11:30:23 +0100

    add negative test

--- tests/NumbersTest.php
+++ tests/NumbersTest.php
@@ -20,6 +20,13 @@ class NumbersTest extends PHPUnit_Framework_TestCase
     $this->assertEquals('1b', Numbers::humanize(1000000000));
     $this->assertEquals('1t', Numbers::humanize(1000000000000));
 
+    $this->assertEquals('-100', Numbers::humanize(-100));
+    $this->assertEquals('-1k', Numbers::humanize(-1000));
+    $this->assertEquals('-12.5k', Numbers::humanize(-12500));
+    $this->assertEquals('-1m', Numbers::humanize(-1000000));
+    $this->assertEquals('-1b', Numbers::humanize(-1000000000));
+    $this->assertEquals('-1t', Numbers::humanize(-1000000000000));
+
     $this->assertEquals('100', Numbers::humanize(100, true));
     $this->assertEquals('1k', Numbers::humanize(1000, true));
     $this->assertEquals('12.5k', Numbers::humanize(12500, true));
