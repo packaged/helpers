5ab34414c4145a83c8c13fbd2d92c8106cf4aef6
Brooke Bryan <brooke@bajb.net>
2017-02-21 15:10:21 +0000

    Allow name to be specified to improve name extraction

--- src/EmailAddress.php
+++ src/EmailAddress.php
@@ -12,6 +12,7 @@ class EmailAddress
   protected $_lower;
   protected $_base;
   protected $_extension;
+  protected $_providedName = ['', '', ''];
   protected $_calculated = false;
   protected $_raw;
 
@@ -24,7 +25,12 @@ class EmailAddress
   {
     $this->_calculated = false;
     $this->_raw = $email;
-    $this->_calculate();
+    return $this;
+  }
+
+  public function setName($first = '', $middle = '', $last = '')
+  {
+    $this->_providedName = [$first, $middle, $last];
     return $this;
   }
 
@@ -54,33 +60,85 @@ class EmailAddress
       $this->_username = strtolower($this->_base);
 
       $this->_lower = strtolower($this->_raw);
+      $this->_calculateName();
       $this->_calculated = true;
+    }
+  }
 
-      $this->_fullName = ucwords(
-        Strings::splitOnCamelCase(
-          Strings::splitOnUnderscores(str_replace('.', ' ', $this->_base))
-        )
+  protected function _calculateName()
+  {
+    list($first, $middle, $last) = $this->_providedName;
+    $first = strtolower($first);
+
+    $this->_fullName = Strings::splitOnCamelCase(
+      Strings::splitOnUnderscores(str_replace('.', ' ', $this->_base))
+    );
+
+    $this->_fullName = strtolower(
+      preg_replace('([0-9])', ' ', $this->_fullName)
+    );
+
+    if(strlen($first) > 0)
+    {
+      $this->_fullName = str_replace(
+        $first,
+        $first . ' ',
+        $this->_fullName
       );
+    }
 
-      $this->_fullName = preg_replace('([0-9])', ' ', $this->_fullName);
-      $this->_fullName = trim(preg_replace('!\s+!', ' ', $this->_fullName));
-
-      $nameParts = explode(' ', $this->_fullName);
-      switch(count($nameParts))
+    if(strlen($first) > 1 && strlen($this->_fullName) > 1)
+    {
+      if($this->_fullName[0] == $first[0] && $this->_fullName[1] != $first[1])
       {
-        case 1:
-          $this->_firstName = $nameParts[0];
-          break;
-        case 2:
-          $this->_firstName = $nameParts[0];
-          $this->_lastName = $nameParts[1];
-          break;
-        default:
-          $this->_firstName = array_shift($nameParts);
-          $this->_lastName = array_pop($nameParts);
-          $this->_middleName = implode(' ', $nameParts);
-          break;
+        $this->_fullName = substr($this->_fullName, 0, 1)
+          . ' ' . substr($this->_fullName, 1);
       }
+    }
+
+    $this->_fullName = ucwords(
+      trim(preg_replace('!\s+!', ' ', $this->_fullName))
+    );
+
+    $nameParts = explode(' ', $this->_fullName);
+    switch(count($nameParts))
+    {
+      case 1:
+        $this->_firstName = $nameParts[0];
+        break;
+      case 2:
+        $this->_firstName = $nameParts[0];
+        $this->_lastName = $nameParts[1];
+        break;
+      default:
+        $this->_firstName = array_shift($nameParts);
+        $this->_lastName = array_pop($nameParts);
+        $this->_middleName = implode(' ', $nameParts);
+        break;
+    }
+
+    if(strlen($first) > strlen($this->_firstName)
+      || (substr($first, 0, 1) != substr($this->_firstName, 0, 1)
+        && !empty($first))
+    )
+    {
+      $this->_firstName = ucwords(strtolower($first));
+    }
+
+    if(strlen($last) > strlen($this->_lastName)
+      || (substr($last, 0, 1) != substr($this->_lastName, 0, 1)
+        && !empty($last))
+    )
+    {
+      $this->_lastName = ucwords(strtolower($last));
+    }
+
+    if(strlen($middle) > strlen($this->_middleName)
+      || (substr($middle, 0, 1) != substr($this->_middleName, 0, 1)
+        && !empty($middle))
+    )
+    {
+      $this->_middleName = ucwords(strtolower($middle));
     }
   }
 
@@ -135,7 +193,10 @@ class EmailAddress
   public function getFullName()
   {
     $this->_calculate();
-    return $this->_fullName;
+    return implode(
+      ' ',
+      array_filter([$this->_firstName, $this->_middleName, $this->_lastName])
+    );
   }
 
   /**
