525311d813ce879f3b4e7749ff1c352c4f81b7d4
Tom Kay <oridan82@gmail.com>
2020-08-11 11:40:58 +0100

    merge TimeHelper into DateTimeHelper

--- src/TimeHelper.php
+++ src/TimeHelper.php
@@ -1,63 +1,9 @@
 <?php
 namespace Packaged\Helpers;
 
-use function floor;
-use function hexdec;
-use function microtime;
-use function str_pad;
-use function substr;
-use const STR_PAD_LEFT;
-
-class TimeHelper
+/**
+ * @deprecated
+ */
+class TimeHelper extends DateTimeHelper
 {
-  const SQL_DATETIME = 'Y-m-d H:i:s';
-  const SQL_DATE = 'Y-m-d';
-  const SQL_TIME = 'H:i:s';
-
-  /**
-   * Convert a unique ID to a microtime
-   *
-   * @param      $uniqid
-   * @param bool $hasEntropy
-   *
-   * @return string
-   */
-  public static function uniqidToMilliseconds($uniqid, $hasEntropy = false)
-  {
-    if($hasEntropy)
-    {
-      $uniqid = substr($uniqid, 0, -10);
-    }
-    $microtime = (int)hexdec(substr($uniqid, -5));
-    $microtime = str_pad($microtime, 6, '0', STR_PAD_LEFT);
-    $timestamp = (int)hexdec(substr($uniqid, -13, -5));
-    return (int)(($timestamp . '.' . $microtime) * 1000);
-  }
-
-  /**
-   * Get the current time in milliseconds since the UNIX epoch
-   *
-   * @return int
-   */
-  public static function milliseconds()
-  {
-    return (int)floor(microtime(true) * 1000);
-  }
-
-  /**
-   * Convert a millisecond (or finer) timestamp to a UNIX timestamp
-   * This crude method will work until 2037
-   *
-   * @param int $time
-   *
-   * @return int
-   */
-  public static function toSeconds($time)
-  {
-    while($time > 2147483647) // 2^31 - 1
-    {
-      $time = floor($time / 1000);
-    }
-    return (int)$time;
-  }
 }
