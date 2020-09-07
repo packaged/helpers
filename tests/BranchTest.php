b9a5067b6f08c86d6a287f220ee3865f53ae1209
Tom Kay <oridan82@gmail.com>
2020-09-07 20:08:27 +0100

    support iterate and flatten, fix issue with null item IDs

--- tests/BranchTest.php
+++ tests/BranchTest.php
@@ -15,20 +15,22 @@ class BranchTest extends TestCase
       ['id' => 0, 'parentId' => null, 'key' => 'value', 'data' => ['root data']],
       ['id' => 1, 'parentId' => null, 'key' => 'value', 'data' => ['root data']],
       ['id' => 2, 'parentId' => 1, 'key' => 'value', 'data' => ['1 child 1']],
-      ['id' => 3, 'parentId' => 1, 'key' => 'value', 'data' => ['1 child 2']],
-      ['id' => 4, 'parentId' => 2, 'key' => 'value', 'data' => ['2 child 1']],
+      ['id' => 3, 'parentId' => 2, 'key' => 'value', 'data' => ['2 child 1']],
+      ['id' => 4, 'parentId' => 1, 'key' => 'value', 'data' => ['1 child 2']],
     ];
 
     $tree = Branch::trunk()->iHydrate($test, 'id', 'parentId');
     $this->assertTrue($tree->hasChildren());
     $this->assertEquals(2, count($tree->getChildren()));
     $this->assertEquals(
-      '[{"object":{"id":0,"parentId":null,"key":"value","data":["root data"]},"children":[]},{"object":{"id":1,"parentId":null,"key":"value","data":["root data"]},"children":[{"object":{"id":2,"parentId":1,"key":"value","data":["1 child 1"]},"children":[{"object":{"id":4,"parentId":2,"key":"value","data":["2 child 1"]},"children":[]}]},{"object":{"id":3,"parentId":1,"key":"value","data":["1 child 2"]},"children":[]}]}]',
+      '[{"object":{"id":0,"parentId":null,"key":"value","data":["root data"]},"children":[]},{"object":{"id":1,"parentId":null,"key":"value","data":["root data"]},"children":[{"object":{"id":2,"parentId":1,"key":"value","data":["1 child 1"]},"children":[{"object":{"id":3,"parentId":2,"key":"value","data":["2 child 1"]},"children":[]}]},{"object":{"id":4,"parentId":1,"key":"value","data":["1 child 2"]},"children":[]}]}]',
       json_encode($tree)
     );
     $this->assertNull($tree->getItem());
     $this->assertNull($tree->getParent());
     $this->assertEquals($tree, $tree->getChildren()[0]->getParent());
+
+    $this->assertEquals($test, $tree->flatten());
   }
 
   public function testStdClassTree()
@@ -93,5 +95,40 @@ class BranchTest extends TestCase
     $this->assertNull($tree->getParent());
     $this->assertEquals($tree, $tree->getChildren()[0]->getParent());
     $this->assertInstanceOf(TreeThing::class, $tree->getChildren()[0]->getItem());
+  }
+
+  public function testMissingKey()
+  {
+    $root = Objects::create(TreeThing::class, [0, null, 'value', ['root data']]);
+    $child1 = Objects::create(TreeThing::class, [1, 0, 'value', ['child 1']]);
+    $child1child = Objects::create(TreeThing::class, [null, 1, 'value', ['child 1 child']]);
+    $child2 = Objects::create(TreeThing::class, [2, 0, 'value', ['child 2']]);
+    $child2child = Objects::create(TreeThing::class, [null, 2, 'value', ['child 2 child']]);
+
+    $test = [
+      $root,
+      $child1,
+      $child1child,
+      $child2,
+      $child2child,
+    ];
+
+    $tree = Branch::trunk()->mHydrate($test, 'getId', 'getParentId');
+    $this->assertEquals(
+      '[{"object":{"id":0,"parentId":null,"key":"value","data":["root data"]},"children":[{"object":{"id":1,"parentId":0,"key":"value","data":["child 1"]},"children":[{"object":{"id":null,"parentId":1,"key":"value","data":["child 1 child"]},"children":[]}]},{"object":{"id":2,"parentId":0,"key":"value","data":["child 2"]},"children":[{"object":{"id":null,"parentId":2,"key":"value","data":["child 2 child"]},"children":[]}]}]}]',
+      json_encode($tree)
+    );
+
+    $this->assertEquals($test, $tree->flatten());
+
+    // test level order
+    $test = [
+      $root,
+      $child1,
+      $child2,
+      $child1child,
+      $child2child,
+    ];
+    $this->assertEquals($test, $tree->flatten(true));
   }
 }
